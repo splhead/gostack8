6f4bf6a02ca530cf35bfea1460d3cf7adfd3852c
Silas Pinho Ladislau <splhead@gmail.com>
2019-08-23 12:29:28 -0400

    modulo02 done

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,16 +1,17 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Testando da Silva',
-    email: 'diego@rocket.com.br',
-    password_hash: '12098u34534',
-  });
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
-  return res.json(user);
-});
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
