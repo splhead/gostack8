8f5f99b655907020bb0606fd70b48d75b3bccda9
Silas Pinho Ladislau <splhead@gmail.com>
2019-09-02 13:31:18 -0400

    meetup - checkpoint

---
+++ meetapp/src/app/controllers/MeetupController.js
@@ -0,0 +1,40 @@
+import * as Yup from 'yup';
+import { isBefore, startOfHour, parseISO } from 'date-fns';
+
+import Meetup from '../models/Meetup';
+
+class MeetupController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { title, description, location, date, banner_id } = req.body;
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past date are not permitted.' });
+    }
+
+    const meetup = Meetup.create({
+      title,
+      description,
+      location,
+      date,
+      banner_id,
+      user_id: req.userId,
+    });
+
+    return res.json(meetup);
+  }
+}
+
+export default new MeetupController();
---
+++ meetapp/src/app/models/Meetup.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Meetup extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        description: Sequelize.STRING,
+        location: Sequelize.STRING,
+        date: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'banner_id' });
+  }
+}
+
+export default Meetup;
