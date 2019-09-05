b1e6a268c49eae16722a5ad949ee11e288f814bb
Silas Pinho Ladislau <splhead@gmail.com>
2019-09-05 13:19:30 -0400

    React - ciclo de vida

---
+++ modulo04/src/components/TechList.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+import TechItem from './TechItem';
+
+class TechList extends Component {
+  state = {
+    newTech: '',
+    techs: ['Node.js', 'ReactJS', 'React Native'],
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: '',
+    });
+  };
+
+  handleDelete = tech => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
+          <TechItem />
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
+
+export default TechList;
