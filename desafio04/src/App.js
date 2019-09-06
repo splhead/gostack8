37b473a0b0162e08f7030f838dc8faf2c88af141
Silas Pinho Ladislau <splhead@gmail.com>
2019-09-06 13:08:29 -0400

    React - desafio 04 checkpoint

---
+++ desafio04/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Header from './components/Header';
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ desafio04/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class components extends Component {
+  render() {
+    return (
+      <header>
+        <nav>
+          <p>facebook</p>
+          <span>Meu perfil</span>
+        </nav>
+      </header>
+    );
+  }
+}
